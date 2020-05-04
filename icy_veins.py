ad3579119d74c1cc0cc780720a1aff6af96ff198
Derek Dang <dqdang17@gmail.com>
2020-05-03 18:38:07 -0700

    bug fix with lower case letters

--- icy_veins.py
+++ icy_veins.py
@@ -87,6 +87,12 @@ def check_for_item(item, list_of_items):
     if len(results) > 0:
         b_string = ""
         for each_item in results:
+            split_string = each_item.split()
+            for i in range(len(split_string)):
+                if split_string[i] == "of" or split_string[i] == "the":
+                    continue
+                split_string[i] = split_string[i].capitalize()
+            each_item = " ".join(split_string)
             b_string += "{}\n".format(each_item)
             for build_link in list_of_items[each_item]:
                 b_string += "{}\n".format(build_link)
